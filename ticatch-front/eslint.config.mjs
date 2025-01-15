a6ef786ebcfbe3602910006a73fb7b57b1b4a73b
Junwoo Song <sjw534237@gmail.com>
2025-01-15 22:56:10 +0900

    chore: prettier, eslint 설정 수정

--- ticatch-front/eslint.config.mjs
+++ ticatch-front/eslint.config.mjs
@@ -6,19 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-redundant-type-constituents': 'warn',
-      '@next/next/no-img-element': 'warn'
-    }
-  }
+      '@next/next/no-img-element': 'warn',
+    },
+  },
 ];
 
 export default eslintConfig;
