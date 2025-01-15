77f3148618c1866f17925f539151d5bbd41cb9e2
Junwoo Song <sjw534237@gmail.com>
2025-01-15 21:52:00 +0900

    chore: 타입스크립트, next lint rule 추가

--- ticatch-front/eslint.config.mjs
+++ ticatch-front/eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-redundant-type-constituents': 'warn',
+      '@next/next/no-img-element': 'warn'
+    }
+  }
 ];
 
 export default eslintConfig;
