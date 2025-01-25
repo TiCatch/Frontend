e2b3720eabc84d878c145c6cd852c59cc00fc4ca
Hyunsoo <imapples@naver.com>
2025-01-25 16:13:34 +0900

    feat: 로그인 페이지 UI 적용

--- ticatch-front/app/login/page.tsx
+++ ticatch-front/app/login/page.tsx
@@ -2,6 +2,11 @@
 
 import { useEffect } from 'react';
 import { isTokenExpired } from 'api';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import logo from 'public/logo.svg';
+import kakao from 'public/kakao.svg';
 
 const LoginPage = () => {
   const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID as string;
@@ -13,14 +18,40 @@ const LoginPage = () => {
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken && isTokenExpired(accessToken)) {
-      console.warn('만료된 토큰 발견. localStorage 정리 중...');
       localStorage.removeItem('accessToken');
     }
   }, []);
 
   return (
-    <div>
-      <a href={KAKAO_AUTH_URL}>로그인 버튼</a>
+    <div className="flex h-screen w-full select-none flex-col items-center justify-center gap-[8%]">
+      <div className="flex items-end gap-[4px] text-2xl text-gray-500">
+        {/* TiCath 로고. 클릭 시 메인으로 이동 */}
+        <Link href="/" className="relative mr-[4px] h-[36px] w-[191px]">
+          <Image src={logo} alt="TiCatch" fill className="object-cover" />
+        </Link>
+        를 사용하기 위해 로그인하세요
+        <div className="mb-[4px] h-[8px] w-[8px] animate-blink rounded-full" />
+      </div>
+      <div className="text-center text-xs text-gray-300">
+        카카오 계정으로 간편 로그인/회원가입
+        {/* 카카오 로그인 버튼 */}
+        <a
+          href={KAKAO_AUTH_URL}
+          className="relative mt-[16px] flex h-[80px] w-[480px] items-center rounded-[16px] bg-[#fee500] px-[32px] py-[28px] shadow-simple">
+          {/* 카카오 로고 */}
+          <Image
+            src={kakao}
+            alt="kakao icon"
+            width={36}
+            height={34}
+            className="flex-none"
+          />
+          <div className="flex-1 text-center text-xl text-black">
+            카카오로 1초만에 로그인/회원가입
+          </div>
+          <div className="absolute left-0 top-0 h-[80px] w-[480px] cursor-pointer rounded-[16px] bg-blackTip-0.04 opacity-0 transition-opacity duration-200 hover:opacity-100"></div>
+        </a>
+      </div>
     </div>
   );
 };
