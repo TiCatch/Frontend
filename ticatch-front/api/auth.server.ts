765972fc308896d4f955d628968bc8a8596bfaf2
Hyunsoo <imapples@naver.com>
2025-02-15 17:28:46 +0900

    feat: 로그인 관련 api 추가

---
+++ ticatch-front/api/auth.server.ts
@@ -0,0 +1,12 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+/**
+ * 서버 단에서 로그인 상태 확인 (cookies의 refreshToken 여부로 판단)
+ * @returns 로그인 여부
+ */
+export const getUserStatusServer = async () => {
+  const cookieStore = cookies();
+  return (await cookieStore).has('refreshToken');
+};
