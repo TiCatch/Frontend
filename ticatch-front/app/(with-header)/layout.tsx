9149ba2646630a02cda13e36f399e56bd8fb744e
Junwoo Song <sjw534237@gmail.com>
2025-02-21 16:33:16 +0900

    fix: 헤더 오류 수정 및 레이아웃 수정

--- ticatch-front/app/(with-header)/layout.tsx
+++ ticatch-front/app/(with-header)/layout.tsx
@@ -1,13 +1,17 @@
+'use client';
 import Header from '@components/Header';
+import { usePathname } from 'next/navigation';
 
 export default function HeaderLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isTicketingPage = pathname.includes('/ticketing');
   return (
     <div className="container">
-      <Header />
+      {!isTicketingPage && <Header />}
       {children}
     </div>
   );
--- ticatch-front/components/Header.tsx
+++ ticatch-front/components/Header.tsx
@@ -4,13 +4,10 @@ import { logoImage } from '@constants/imagePath';
 import { useLogout, useUserStatus } from '@hooks';
 import Image from 'next/image';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 const Header = () => {
   const { data: isLoggedIn } = useUserStatus();
   const logout = useLogout();
-  const router = useRouter();
 
   return (
     <header className="flex justify-between py-5">
@@ -18,7 +15,7 @@ const Header = () => {
         <Image src={logoImage} alt="Header" width={128} height={24} />
       </Link>
       {isLoggedIn ? (
-        <button onClick={logout}>로그아웃</button>
+        <button onClick={() => logout.mutate()}>로그아웃</button>
       ) : (
         <a href="/login">로그인</a>
       )}
