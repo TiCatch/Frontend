858f0d6d2df66672bb9cd77bc426f4bff36e8ee1
Hyunsoo <imapples@naver.com>
2025-03-03 16:17:29 +0900

    fix: 미들웨어 refresh token 철자 수정

--- ticatch-front/middleware.ts
+++ ticatch-front/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const refreshToken = request.cookies.get('refreshToken')?.value;
+  const refreshToken = request.cookies.get('refresh-token')?.value;
 
   if (!refreshToken) {
     return NextResponse.redirect(new URL('/login', request.url));
