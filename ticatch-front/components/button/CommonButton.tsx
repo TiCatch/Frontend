d3789af47c320a3bd5324a4fa039a0d2fd83a4d7
Hyunsoo <imapples@naver.com>
2025-05-28 22:33:08 +0900

    feat: commonbutton에 small 타입 추가

--- ticatch-front/components/button/CommonButton.tsx
+++ ticatch-front/components/button/CommonButton.tsx
@@ -6,6 +6,7 @@ interface ButtonProps {
   isDisabled?: boolean;
   onClick?: () => void;
   icon?: React.JSX.Element;
+  type?: string;
 }
 
 const CommonButton: React.FC<ButtonProps> = ({
@@ -16,12 +17,13 @@ const CommonButton: React.FC<ButtonProps> = ({
   onClick,
   borderColor = 'border-transparent',
   icon,
+  type = 'medium',
 }) => {
   return (
     <button
       onClick={onClick}
       disabled={isDisabled}
-      className={`relative flex h-[46px] min-w-[160px] items-center justify-center whitespace-nowrap ${textColor} border ${borderColor} rounded-[8px] px-[32px] py-[14px] text-m ${isDisabled && 'cursor-not-allowed'} btn-hover-overlay overflow-hidden ${backgroundColor} gap-2`}>
+      className={`relative flex h-[46px] ${type === 'small' ? 'min-w-[66px]' : 'min-w-[160px]'} items-center justify-center whitespace-nowrap ${textColor} border ${borderColor} rounded-[8px] ${type === 'small' ? 'px-[14px]' : 'px-[32px]'} py-[14px] text-m ${isDisabled && 'cursor-not-allowed'} btn-hover-overlay overflow-hidden ${backgroundColor} gap-2`}>
       {icon}
       {title}
     </button>
