ac0b481b5659da15bba04dfff1c0db0bb7fda2c1
Junwoo Song <sjw534237@gmail.com>
2025-05-30 22:13:42 +0900

    feat: 헤더 노출 조건 컴포넌트로 이동

--- ticatch-front/app/(with-header)/layout.tsx
+++ ticatch-front/app/(with-header)/layout.tsx
@@ -1,17 +1,9 @@
-'use client';
 import Header from '@components/Header';
-import { usePathname } from 'next/navigation';
 
-export default function HeaderLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const pathname = usePathname();
-  const isTicketingPage = pathname.includes('/ticketing');
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container">
-      {!isTicketingPage && <Header />}
+      <Header />
       {children}
     </div>
   );
