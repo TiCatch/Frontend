8182bbd6bbb80df0ab82ff14366ebd5970dd2806
Hyunsoo <imapples@naver.com>
2025-06-22 20:39:11 +0900

    feat: 메인과 사이드 광고 컴포넌트 생성

---
+++ ticatch-front/components/Ad/SideAd.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import AdTemplate from './AdTemplate';
+
+const SideAd = () => {
+  return <AdTemplate width={250} height={250} unit={'DAN-gH7PBgNERAGfuKjg'} />;
+};
+
+export default SideAd;
